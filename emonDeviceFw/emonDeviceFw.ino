78f530232ec337ff615e483cb81876aadee03d29
cziter15 <chris.streh@gmail.com>
2021-03-14 18:49:47 +0100

    Update to support new lib

--- emonDeviceFw/emonDeviceFw.ino
+++ emonDeviceFw/emonDeviceFw.ino
@@ -1,24 +1,10 @@
 #include "src/EnergyMonitor.h"
 #include "src/config/EnergyMonitorConfig.h"
 
-#ifdef ESP32
-#include "SPIFFS.h"
-#else
-#include "FS.h"
-#endif
-
-#include "user_interface.h"
-
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	uint32_t chipId = ESP.getChipId();
-	auto chipIdBytes = (unsigned char*)&chipId;
-
-	uint8_t mac[6]{ 0xfa, 0xf1, chipIdBytes[0], chipIdBytes[1], chipIdBytes[2], chipIdBytes[3]};
-	wifi_set_macaddr(STATION_IF, mac);
-
-	SPIFFS.begin();
+	ksf::initKsfFramework();
 }
 
 // the loop function runs over and over again until power down or reset
